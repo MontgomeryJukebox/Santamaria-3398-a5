6591320838b10d95c96e117938043b59f4928b58
David Santamaria <davidsantamaria1245@gmail.com>
2021-07-24 11:18:18 -0400

    add csv import support

--- src/main/java/ucf/assignments/exercise56/Importer.java
+++ src/main/java/ucf/assignments/exercise56/Importer.java
@@ -9,11 +9,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.util.converter.BigDecimalStringConverter;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 /* Our importer for handling TODO list imports from files */
 public class Importer {
@@ -37,7 +41,19 @@ public class Importer {
 
     public ObservableList<Item> importCSV(String filename) throws IOException {
         ObservableList<Item> ret = FXCollections.observableArrayList();
-        // TODO
+        Reader reader = Files.newBufferedReader(Paths.get(rootDir + "/" + filename));
+        Scanner in = new Scanner(reader);
+        try {
+            while (in.hasNextLine()) {
+                String sn = in.next();
+                String name = in.next();
+                String value = in.next();
+                Item i = new Item(sn, name, new BigDecimalStringConverter().fromString(value));
+                ret.add(i);
+            }
+        } catch (NoSuchElementException e) {
+            // do nothing
+        }
         return ret;
     }
 
